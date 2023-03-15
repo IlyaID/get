1337257d89dc7770c3b32b5ffda8fd6e148b98e8
IlyaID <ivanov.id@phystech.edu>
2023-03-15 12:12:49 +0300

    Update

--- DAC/4-3.py
+++ DAC/4-3.py
@@ -6,10 +6,9 @@ def decimal2binary(value, n):
     return [int(element) for element in bin(value)[2:].zfill(n)]
 
 GPIO.setmode(GPIO.BCM)
-DAC = [26, 19, 13, 6, 5, 11, 9, 10]
-GPIO.setup(DAC, GPIO.OUT)
 GPIO.setup(2, GPIO.OUT)
-pwm = GPIO.PWM(26, 25)
+pwm = GPIO.PWM(2, 1000)
+
 pwm.start(0)
 
 
@@ -18,7 +17,10 @@ try:
     while(1):
         input_value = input('Please enter number\n')
         pwm.ChangeDutyCycle(int(input_value))
+        print("{:.4f}".format(int(input_value)/100*3.3))
+        
         
 finally:
-    GPIO.output(DAC, GPIO.LOW)
-    GPIO.cleanup(DAC)
+    GPIO.output(2, GPIO.LOW)
+    
+    GPIO.cleanup()
