d3ed655036add4a4643da76e24065da84d6caf57
Arina Mironova <mironova.as@phystech.edu>
2023-04-05 10:46:52 +0300

    Hello

--- ADC/5_3.py
+++ ADC/5_3.py
@@ -9,13 +9,15 @@ maxVoltage = 3.3
 troykaModule = 17
 comparator = 4
 weight = [128, 64, 32, 16, 8, 4, 2, 1]
-
+LEDS = [24, 25, 8, 7, 12, 16, 20, 21]
+LEDS1 = [21, 20, 16, 12, 7, 8, 25, 24]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DAC, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup(troykaModule, GPIO.OUT, initial = GPIO.HIGH)
 GPIO.setup(comparator, GPIO.IN)
-
+GPIO.setup(LEDS, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(LEDS1, GPIO.OUT, initial = GPIO.LOW)
 
 def decimal2binary(value, n):
     return [int(element) for element in bin(value)[2:].zfill(n)]
@@ -31,12 +33,17 @@ def adc():
     for value in range(8):
         signal = decimal2binary(summary + weight[value],8)
         GPIO.output(DAC, signal)
-        time.sleep(0.1)
+        time.sleep(0.01)
         comparatorValue = GPIO.input(comparator)
         if(comparatorValue == 1):
             summary += weight[value]
         signal = decimal2binary(summary,8)
         voltage = summary / levels * maxVoltage
+        for value in range(8):
+            if (value < summary / 255 * 8):
+                GPIO.output(LEDS[value], 1)
+            else:
+                GPIO.output(LEDS[value], 0)
         print("Summary value = {:^3} -> {}, output voltage = {:.2f}".format(summary, signal, voltage))
 try:
     while True:
